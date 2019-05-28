3a799e549ebe1875c53b279c11c7d22da97f91c2
Paul Rentschler <paul@rentschler.ws>
2019-05-27 22:24:45 -0400

    Convert to a Notify class

--- notify.py
+++ notify.py
@@ -1,29 +1,42 @@
 from cata import Cata
 
 
-route = 'B'
-stops = [356, 565, 1]
 
-cata = Cata()
-for stop in stops:
-    results = {}
-    items = cata.departures(stop)
-    for item in items:
-        route = item.route
-        if route not in results.keys():
-            results[route] = []
-        results[route].append(item)
-    print('\n')
-    print('last updated: {}'.format(cata.last_updated))
-    for route, departures in results.items():
-        print('{} -- {}'.format(
-            departures[0].route_name,
-            departures[0].bus.title(),
-        ))
-        for d in departures:
-            print('  - {}/{} {}'.format(
-                d.scheduled,
-                d.estimated,
-                d.delta_label,
-            ))
+class Notify(object):
+    def __init__(self, stops, route=None):
+        self.stops = stops
+        self.route = route
+        self.stop_departures = {}
+        self.cata = Cata()
+        for stop in self.stops:
+            results = {}
+            items = self.cata.departures(stop, self.route)
+            for item in items:
+                route = item.route
+                if route not in results.keys():
+                    results[route] = []
+                results[route].append(item)
+            self.stop_departures[stop] = results
+
+    def cli(self):
+        for stop, results in self.stop_departures.items():
+            print('\n')
+            print('Stop: {}'.format(stop))
+            print('last updated: {}'.format(self.cata.last_updated))
+            for route, departures in results.items():
+                print('{} -- {}'.format(
+                    departures[0].route_name,
+                    departures[0].bus.title(),
+                ))
+                for d in departures:
+                    print('  - {}/{} {}'.format(
+                        d.scheduled,
+                        d.estimated,
+                        d.delta_label,
+                    ))
+
+
+if __name__ == '__main__':
+    notifier = Notify([356, 565])
+    notifier.cli()
 
